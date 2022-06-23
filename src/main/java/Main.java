10563dadf720b6d5508445ce4735e27204e80008
Conner Gleason <connereg@optonline.net>
2022-06-23 12:27:30 -0400

    Completed Lab, no issues, will return to expand functionality

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,22 @@
+// import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
         // your code here
+        boolean trueVar = true;
+        boolean falseVar = false;
+
+        System.out.println("Truth Table for && Operator");
+        System.out.println("true && true = " + (trueVar && trueVar));
+        System.out.println("true && false = " + (trueVar && falseVar));
+        System.out.println("false && false = " + (falseVar && falseVar));
+        System.out.println("false && true = " + (falseVar && trueVar));
+
+        System.out.println("Truth Table for || Operator");
+        System.out.println("true || true = " + (trueVar || trueVar));
+        System.out.println("true || false = " + (trueVar || falseVar));
+        System.out.println("false || false = " + (falseVar || falseVar));
+        System.out.println("false || true = " + (falseVar || trueVar));       
     }
 }
